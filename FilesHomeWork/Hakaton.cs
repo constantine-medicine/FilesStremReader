815afecd2f270c8e2a9d92b10c7c11075a608f06
Константин <rakin.rolll@inbox.ru>
2020-11-24 16:28:51 +0300

    Добавьте файлы проекта.

---
+++ FilesHomeWork/Hakaton.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesHomeWork
+{
+    class Hakaton
+    {
+        /// <summary>
+        /// Метод, разбивающий последовательность целых от 0 но N на группы неделимых чисел, группы в последствии записываются 
+        /// в файлы во вновь созданную папку. Метод принимает в себя int N в качестве параметра, переменную типа string как путь для создания папки
+        /// в которую будут сохраняться файлы, возвращает переменную типа int как количество созданных групп.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int SaveGroup(int n, string path)
+        {
+            int count = 0;
+            int[] arr = new int[0];
+            int counterGroup = (int)Math.Log(n, 2) + 1;
+
+            while (counterGroup > 0)
+            {
+                if (n % 2 != 0) arr = new int[n / 2 + 1];
+                if (n % 2 == 0) arr = new int[n / 2];
+
+                for (int i = n / 2 + 1; i <= n; i++)
+                {
+                    arr[count] = i;
+                    count++;
+                }
+
+                Directory.CreateDirectory($@"{path}");
+
+                using (StreamWriter sw = new StreamWriter($@"{path}\group{counterGroup}.txt"))
+                {
+                    foreach (var item in arr)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+
+                count = 0;
+                n /= 2;
+                counterGroup--;
+            }
+            
+            counterGroup = (int)Math.Log(n, 2) + 1;
+            return counterGroup;
+        }
+
+        /// <summary>
+        /// Метод, позволяющий архивировать данные во вновь созданную директорию.
+        /// Принимает в качестве агрумента int n как количество файлов, доступных для архивации, 
+        /// переменную типа string как путь к файлам, которые необоходимо сжимать,
+        /// переменную типа string как путь для создания папки со сжатыми файлами
+        /// </summary>
+        /// <param name="n"></param>
+        public static void ArchiveZip(int n, string pathRead, string pathSave)
+        {
+            Directory.CreateDirectory($@"{pathSave}");
+
+            while (n > 0)
+            {
+                using (FileStream fs = new FileStream($@"{pathRead}\group{n}.txt", FileMode.OpenOrCreate))
+                {
+                    using (FileStream fsSec = File.Create($@"{pathSave}\group{n}.zip"))
+                    {
+                        using (GZipStream gs = new GZipStream(fsSec, CompressionMode.Compress))
+                        {
+                            fs.CopyTo(gs);
+                        }
+                    }
+                }
+                n--;
+            }
+        }
+    }
+}
---
+++ FilesHomeWork/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace FilesHomeWork
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Программа, считывающая из папки некоторое число Number и разбивает последовательность чисел от 0 до Number
+            // на группы неделимых чисел.
+           
+            Console.Write("Введите путь к файлу: ");
+            string pathFile = Convert.ToString(Console.ReadLine());
+            string text = File.ReadAllText($@"{pathFile}");
+            int number = Convert.ToInt32(text);
+
+            Console.Write($"Полученное число: {number}");
+            int countergroup = (int)Math.Log(number, 2) + 1;
+
+            Console.WriteLine("\nВыберите режим работы: \n1 - Показать количество получившихся групп. \n2 - Получить заполненные группы в файле.");
+
+            do
+            {
+                // Выбор режима работы программы
+                bool worker = int.TryParse(Console.ReadLine(), out int result);
+
+                // Первый режим работы (показать количество групп)
+                if (result == 1 && worker)
+                {
+                    DateTime date = new DateTime();
+                    date = DateTime.Now;
+
+                    Console.Write($"\nКоличество групп равно {(int)Math.Log(number, 2) + 1}");
+
+                    DateTime newDate = DateTime.Now;
+                    TimeSpan span = newDate - date;
+
+                    Console.Write($"\nПрограмма выполнилась за {span.TotalSeconds} секунд или {span.TotalMilliseconds} милисекунд.");
+
+                    break;
+                }
+
+                // Второй режим работы (создает файлы и предлагает их архивировать)
+                else if (result == 2 && worker)
+                {
+                    Console.Write("\nУкажите путь в котором будет создана папка для записи файлов: ");
+                    string pathSave = Convert.ToString(Console.ReadLine());
+
+                    DateTime date = new DateTime();
+                    date = DateTime.Now;
+
+                    Hakaton.SaveGroup(number, pathSave);
+
+                    DateTime newDate = DateTime.Now;
+                    TimeSpan span = newDate - date;
+
+                    Console.Write($"\nПрограмма выполнилась за {span.TotalSeconds} секунд или {span.TotalMilliseconds} милисекунд.");
+
+                    Console.Write("\nХотели бы вы заархивировать данные? д/н\n");
+
+                    // Программа задает вопрос об архивации
+                    while (true)
+                    {
+                        string answer = Convert.ToString(Console.ReadLine());
+
+                        if (answer == "д")
+                        {
+                            Console.Write("\nУкажите путь для создания папки с файлами: ");
+                            string pathZip = Convert.ToString(Console.ReadLine());
+
+                            Hakaton.ArchiveZip(countergroup, pathSave, pathZip);
+                            Console.WriteLine("\nДанные успешно сжаты.");
+                            break;
+                        }
+                        else if (answer == "н")
+                        {
+                            Console.WriteLine("Конец программы.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ответом должен быть 'н' или 'д'. Повторите ввод.");
+                        }
+                    }
+                    break;
+                }
+                else Console.WriteLine("Чтобы выбрать режим нажмите 1 или 2. Повторите ввод.");
+            } while (true);
+
+            Console.ReadLine();
+        }
+    }
+}
